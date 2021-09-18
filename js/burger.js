f658bebfc500ea72bbca456e0c652a1b7d083345
REDx-Aster <REDx.V.Belyaev@gmail.com>
2021-09-18 23:09:50 +0300

    update

--- js/burger.js
+++ js/burger.js
@@ -1,3 +1,11 @@
-document.querySelector('.icon-menu').addEventListener('click', function () {
-  document.querySelector('.icon-menu span').classList.toggle('active');
+$(".menu__icon").click(function (event) {
+  $(this).toggleClass("active");
+  $(".menu__body").toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $("body").data("scroll", $(window).scrollTop());
+  }
+  $("body").toggleClass("lock");
+  if (!$(this).hasClass("active")) {
+    $("body,html").scrollTop(parseInt($("body").data("scrol")));
+  }
 });
